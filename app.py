35a625ba7c781244d24d556be1ba334c5a5e7034
Emiliana Candida <emilianacandsilva@gmail.com>
2025-05-26 20:52:05 -0300

    ajustes

--- app.py
+++ app.py
@@ -27,28 +27,54 @@ def enviar_email_com_anexo(destino, assunto, corpo, arquivo_bytes, nome_arquivo)
 @app.route('/', methods=['GET', 'POST'])
 def form():
     if request.method == 'POST':
+        # Formata a data de nascimento para o formato desejado
+        data_nascimento_raw = request.form.get("data_nascimento", "")
+        try:
+            # converte a string para datetime
+            data_nascimento_dt = datetime.strptime(data_nascimento_raw, "%Y-%m-%d")
+            # agora pode usar strftime
+            data_nascimento_formatada = data_nascimento_dt.strftime("%d/%m/%Y")
+        except (ValueError, TypeError):
+            # Se não der pra converter, mantém como está ou define vazio
+            data_nascimento_formatada = data_nascimento_raw
+
+        # Formata CPF
+        cpf_raw = request.form.get("cpf", "")
+        if len(cpf_raw) == 11:
+            cpf_formatado = f"{cpf_raw[:3]}.{cpf_raw[3:6]}.{cpf_raw[6:9]}-{cpf_raw[9:]}"
+
+        else:
+            cpf_formatado = cpf_raw
+
+        rg_raw = request.form.get("rg", "")
+        if len(rg_raw) == 9:
+            rg_formatado = f"{rg_raw[:2]}.{rg_raw[2:5]}.{rg_raw[5:8]}-{rg_raw[8]}"
+        else:
+            rg_formatado = rg_raw
+            
         # 1) Captura dos dados do formulário
         dados = {
-            "<<NOME>>":            request.form.get("nome_completo", ""),
-            "<<ESTADO CIVIL>>":      request.form.get("estado_civil", ""),
-            "<<NACIONALIDADE>>":   request.form.get("nacionalidade", ""),
-            "<<CPF>>":             request.form.get("cpf", ""),
-            "<<DATA NASCIMENTO>>": request.form.get("data_nascimento", ""),
-            "<<PROFISSAO>>":       request.form.get("profissao", ""),
-            "<<CIDADE NASCIMENTO>>": request.form.get("cidade_nascimento", ""),
-            "<<ESTADO NASCIMENTO>>": request.form.get("estado_nascimento", ""),
-            "<<RG>>":                request.form.get("numero_rg", ""),
-            "<<ORGAO EMISSOR>>":     request.form.get("orgao_emissor", ""),
-            "<<ESTADO EMISSOR>>":    request.form.get("estado_emissor", ""),
-            "<<NOME PAI>>":          request.form.get("nome_pai", ""),
-            "<<NOME MAE>>":          request.form.get("nome_mae", ""),
-            "<<CIDADE DOMICILIO>>":  request.form.get("cidade_domicilio", ""),
-            "<<ESTADO DOMICILIO>>":  request.form.get("estado_domicilio", ""),
-            "<<LOGRADOURO DOMICILIO>>":        request.form.get("logradouro", ""),
-            "<<RUA DOMICILIO>>":               request.form.get("rua", ""),
-            "<<NUMERO DOMICILIO>>":            request.form.get("numero", ""),
-            "<<BAIRRO DOMICILIO>>":            request.form.get("bairro", ""),
-            "<<CEP>>":               request.form.get("cep", ""),
+            "<<NOME>>":                             request.form.get("nome_completo", ""),
+            "<<ESTADO CIVIL>>":                     request.form.get("estado_civil", ""),
+            "<<NACIONALIDADE>>":                    request.form.get("nacionalidade", ""),
+            "<<CPF>>":                              cpf_formatado,
+            "<<DATA NASCIMENTO>>":                  data_nascimento_formatada,
+            "<<PROFISSAO>>":                        request.form.get("profissao", ""),
+            "<<CIDADE NASCIMENTO>>":                request.form.get("cidade_nascimento", ""),
+            "<<ESTADO NASCIMENTO>>":                request.form.get("estado_nascimento", ""),
+            "<<RG>>":                               rg_formatado,
+            "<<ORGAO EMISSOR>>":                    request.form.get("orgao_emissor", ""),
+            "<<ESTADO EMISSOR>>":                   request.form.get("estado_emissor", ""),
+            "<<NOME PAI>>":                         request.form.get("nome_pai", ""),
+            "<<NOME MAE>>":                         request.form.get("nome_mae", ""),
+            "<<CIDADE DOMICILIO>>":                 request.form.get("cidade", ""),
+            "<<ESTADO DOMICILIO>>":                 request.form.get("estado", ""),
+            "<<LOGRADOURO DOMICILIO>>":             request.form.get("logradouro", ""),
+            "<<RUA DOMICILIO>>":                    request.form.get("rua", ""),
+            "<<NUMERO DOMICILIO>>":                 request.form.get("numero", ""),
+            "<<BAIRRO DOMICILIO>>":                 request.form.get("bairro", ""),
+            "<<CEP>>":                              request.form.get("cep", ""),
+            "<<GENERO>>": "a"                    if request.form.get("genero", "") == "feminino" else "o",
         }
 
         # 2) Captura data e hora de preenchimento
@@ -87,7 +113,7 @@ def form():
         # Envia email automático pra você
         enviar_email_com_anexo(
             destino='edilson.cs@live.com',
-            assunto='Novo documento gerado',
+            assunto=f"Nova procuração gerada - {nome_pessoa}",
             corpo='Segue em anexo o documento gerado pelo formulário.',
             arquivo_bytes=arquivo_bytes,
             nome_arquivo=nome_saida
